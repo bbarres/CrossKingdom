438d515761ff434565327eb95f1f640aaf2ba979
bbarres <benoit.barres@gmail.com>
2024-11-17 00:02:04 +0100

    more modification

--- CrossKingdom_doseReg.R
+++ CrossKingdom_doseReg.R
@@ -30,7 +30,6 @@ EDcomp(Clo116.m1,c(50,50))
 #LD50 for each replicate
 ED50m1<-ED(Clo116.m1,0.50,type="absolute")
 
-
 #dose response analysis with pooled replicates
 Clo116.m2<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
              weights=(nb_mtot+nb_vi),
@@ -38,23 +37,18 @@ Clo116.m2<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
              fct=LN.3u(),type="binomial")
 #LD50 of the 24-0116-0001 clone
 ED50_116<-ED(Clo116.m2,0.50,type="absolute")
-
 #dose response plot
 plot(Clo116.m2,ylim=c(0,1.1),xlim=c(0,200),
-     main="Clone 24-0116-0001 spirotetramat
-     au spirotétramate",
+     main="Clone 24-0116-0001 spirotetramat",
      type="confidence",ylab="mortality rate",
      las=1)
-
 #adding the observed values for each dose
 plot(Clo116.m2,ylim=c(0,1.1),xlim=c(0,200),
      main=names(table(dataReg116$ech_id))[1],
      type="all",add=TRUE)
-
 #adding the average mortality rate for each dose
 points((nb_mtot/(nb_mtot+nb_vi))~dose,data=dataRegDos116,
        col="red",pch=19)
-
 #adding the LD50 value
 text(x=20,y=0.3,labels=paste("ED50=",round(ED50_116[1],digits=4)))
 
@@ -64,50 +58,42 @@ text(x=20,y=0.3,labels=paste("ED50=",round(ED50_116[1],digits=4)))
 ##############################################################################/
 
 #dose response analyses by replicate
-temp.m3<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
+Clo153.m1<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
              weights=(nb_mtot+nb_vi),
-             data=dataReg1,
+             data=dataReg153,
              curveid=dat_test,
              fct=LN.3u(),type="binomial")
-#plot de la courbe dose-réponse
-plot(temp.m3,ylim=c(0,1.1),xlim=c(0,100),
-     main="Courbe dose réponse du clone 17-0153-0020
-     au spriotétramate par date \n(72h après retrait des femelles)",
+#dose-response plot for the 17-0116-0001 replicate
+plot(Clo153.m1,ylim=c(0,1.1),xlim=c(0,100),
+     main="Clone 17-0153-0020 spirotetramat by replicate
+     (72h after removing female)",
      ylab="mortality rate",legendPos=c(100,0.35),col = T )
-#comparaison des courbes entre dates
-compParm(temp.m3,"e")
-#comparaison DL50 entre dates
-EDcomp(temp.m3,c(50,50))
-#DL 50 pour chaque date
-ED50m1<-ED(temp.m3,0.50,type="absolute")
+#LD50 comparison between replicate
+compParm(Clo153.m1,"e")
+EDcomp(Clo153.m1,c(50,50))
+#LD50 for each replicate
+ED50m1<-ED(Clo153.m1,0.50,type="absolute")
 
-
-# Création de la courbe dose réponse avec
-# le data où les vivants et les morts sont agrégés par date et dose
-temp.m4<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
+#dose response analysis with pooled replicates
+Clo153.m2<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
              weights=(nb_mtot+nb_vi),
-             data=dataRegRep1,
+             data=dataRegRep153,
              fct=LN.3u(),type="binomial")
-#DL50 de la courbe dose réponse avec cumul de dates
-ED50v<-ED(temp.m4,0.50,type="absolute")
-
-#plot de la courbe "moyenne" avec intervalles de confiance
-plot(temp.m4,ylim=c(0,1.1),xlim=c(0,100),
-     main="Courbe moyenne dose-réponse du clone 17-0153-0020
-     au spirotétramate",
+#LD50 of the 17-0153-0020 clone
+ED50v<-ED(Clo153.m2,0.50,type="absolute")
+#dose response plot
+plot(Clo153.m2,ylim=c(0,1.1),xlim=c(0,100),
+     main="Clone 17-0153-0020 spirotetramat",
      type="confidence",ylab="mortality rate",
      las=1)
-
-# plot des taux de mortalité par dose et par date
-plot(temp.m4,ylim=c(0,1.1),xlim=c(0,100),
-     main=names(table(dataReg1$ech_id))[1],
+#adding the observed values for each dose
+plot(Clo153.m2,ylim=c(0,1.1),xlim=c(0,100),
+     main=names(table(dataReg153$ech_id))[1],
      type="all",add=TRUE)
-
-# plot des taux de mortalité par dose
-points((nb_mtot/(nb_mtot+nb_vi))~dose,data=dataRegDos1,
+#adding the average mortality rate for each dose
+points((nb_mtot/(nb_mtot+nb_vi))~dose,data=dataRegDos153,
        col="red",pch=19)
-
-#plot de la valeur de la DL50
+#adding the LD50 value
 text(x=20,y=0.3,labels=paste("ED50=",round(ED50v[1],digits=4)))
 
 
