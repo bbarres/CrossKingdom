b8ab4736a01dedbf79f52a31d17d0566ecc4d32a
bbarres <benoit.barres@gmail.com>
2025-07-26 23:37:27 +0200

    correction of the name of the dataset used

--- CrossKingdom_ACCase.R
+++ CrossKingdom_ACCase.R
@@ -30,16 +30,13 @@ skim(MyzHerbiS)
 #first we investigate the effect of the different factors on total
 #fertility
 hist(MyzHerbiS$Total)
-line(density(MyzHerbiS$Total))
 #analysis of the effect of SA on the mean number of larvae
-temp<-MyzHerbi[MyzHerbi$Dose!="N/2",]
-temp<-drop.levels(temp,reorder=FALSE)
 mod_nblarv<-aov(Total~Active_substance*Dose*Clone
-                +Error(Repetition),data=temp)
+                +Error(Repetition),data=MyzHerbiS)
 summary(mod_nblarv)
 #we can remove the three way interaction
 mod_nblarv<-aov(Total~(Active_substance+Dose+Clone)^2
-                +Error(Repetition),data=temp)
+                +Error(Repetition),data=MyzHerbiS)
 summary(mod_nblarv)
 
 emm<-emmeans(mod_nblarv,~(Active_substance+Dose+Clone)^2)
@@ -49,15 +46,16 @@ plot(emm)
 
 #using the afex package
 #first we need an ID for the different individual
-temp$ID<-paste(temp$Clone,temp$Active_substance,temp$Dose,sep="_")
+MyzHerbiS$ID<-paste(MyzHerbiS$Clone,MyzHerbiS$Active_substance,
+                    MyzHerbiS$Dose,sep="_")
 mod_afex<-aov_car(Total~((Active_substance+Dose+Clone)^2+
                            Error(ID/Repetition)),
-                  data=temp)
+                  data=MyzHerbiS)
 mod_afex
 mod_afex<-aov_car(Total~(Active_substance+Dose+Clone+
                            Active_substance:Dose+Dose:Clone+
                            Error(ID/Repetition)),
-                  data=temp)
+                  data=MyzHerbiS)
 mod_afex
 afex_plot(mod_afex,"Dose","Active_substance","Clone")
 afex_plot(mod_afex,"Dose",panel=~Clone+Active_substance)
@@ -91,7 +89,7 @@ pairs(emmDoByAS)
 #same kind of analysis but in a glmm framework
 mmod_nblarv<-lme(Total~Active_substance*Dose*Clone,
                  random= ~1|Repetition,
-                 data=temp,method="ML")
+                 data=MyzHerbiS,method="ML")
 summary(mmod_nblarv)
 mmod_nblarv.1<-update(mmod_nblarv,~. -Active_substance:Dose:Clone)
 summary(mmod_nblarv.1)
@@ -118,7 +116,7 @@ AIC(mmod_nblarv.1)
 
 #same model but using lmer
 mmod_nblarv.1bis<-lmer(Total~(Active_substance+Dose+Clone)^2+(1|Repetition),
-                       data=temp,REML=FALSE)
+                       data=MyzHerbiS,REML=FALSE)
 plot(mmod_nblarv.1bis)
 plot(mmod_nblarv.1bis,Total~fitted(.))
 Anova(mmod_nblarv.1bis,type="III")
@@ -134,7 +132,7 @@ vif(mmod_nblarv.1bis) #should be <2.2 (sqrt(5)) or at least <3.2 (sqrt(10))
 #generalized model: glmm with poisson distribution
 mmod_nblarv.1ter<-glmmTMB(Total~(Active_substance+Dose+Clone)^2+
                             (1|Repetition),
-                          data=temp,REML=FALSE,family=poisson)
+                          data=MyzHerbiS,REML=FALSE,family=poisson)
 summary(mmod_nblarv.1ter)
 Anova(mmod_nblarv.1ter,type="III")
 testDispersion(mmod_nblarv.1ter) #borderline overdispersion
@@ -144,7 +142,7 @@ AIC(mmod_nblarv.1ter)
 #same analysis with negative binomial
 mmod_nblarv.1qat<-glmmTMB(Total~(Active_substance+Dose+Clone)^2+
                             (1|Repetition),
-                          data=temp,REML=FALSE,
+                          data=MyzHerbiS,REML=FALSE,
                           family=nbinom1) #similar to a quasipoisson
 summary(mmod_nblarv.1qat)
 Anova(mmod_nblarv.1qat,type="III")
@@ -182,8 +180,9 @@ afex_plot(mmod_nblarv.1qat,"Dose",panel=~Clone+Active_substance)
 afex_plot(mmod_nblarv.1qat,"Dose",panel=~Active_substance)
 
 #figures for the effect of AS on the mean number of larvae
-interaction.plot(temp$Dose,temp$Active_substance,temp$Total,las=1)
-boxplot(Total~Clone+Dose+Active_substance,data=temp,las=1,
+interaction.plot(MyzHerbiS$Dose,MyzHerbiS$Active_substance,
+                 MyzHerbiS$Total,las=1)
+boxplot(Total~Clone+Dose+Active_substance,data=MyzHerbiS,las=1,
         at=c(((1+6*0):(6*1))+2*0,
              ((1+6*1):(6*2))+2*1,
              ((1+6*2):(6*3))+2*2,
@@ -194,11 +193,11 @@ boxplot(Total~Clone+Dose+Active_substance,data=temp,las=1,
         border=rep(c(3,4),each=18),
         names=rep(c("NT","NT","N","N","2N","2N"),times=6))
 text(c((0:5)*6+(1:6)*2+1.5),y=99,
-     labels=levels(temp$Active_substance),cex=1.5,
+     labels=levels(MyzHerbiS$Active_substance),cex=1.5,
      col=c(3,3,3,4,4,4))
 #since there is no significant effect of the clone, they can be pooled 
 #together
-boxplot(Total~Dose+Active_substance,data=temp,las=1,
+boxplot(Total~Dose+Active_substance,data=MyzHerbiS,las=1,
         at=c(((1+3*0):(3*1))+2*0,
              ((1+3*1):(3*2))+2*1,
              ((1+3*2):(3*3))+2*2,
@@ -208,25 +207,25 @@ boxplot(Total~Dose+Active_substance,data=temp,las=1,
         border=rep(c(3,4),each=9),
         names=rep(c("NT","N","2N"),times=6))
 text(c((0:5)*3+(1:6)*2),y=99,
-     labels=levels(temp$Active_substance),cex=1.5,
+     labels=levels(MyzHerbiS$Active_substance),cex=1.5,
      col=c(3,3,3,4,4,4))
 
 
 #analysis of the effect of SA on the mean number of larvae per mother
-temp<-MyzHerbi[MyzHerbi$Dose!="N/2",]
-temp<-drop.levels(temp,reorder=FALSE)
+MyzHerbiS<-MyzHerbi[MyzHerbi$Dose!="N/2",]
+MyzHerbiS<-drop.levels(MyzHerbiS,reorder=FALSE)
 mod_nbparMo<-aov(Total/Laying_females~Active_substance*Dose*Clone
-                 +Error(Repetition/Dose/Clone),data=temp)
+                 +Error(Repetition/Dose/Clone),data=MyzHerbiS)
 summary(mod_nbparMo)
 mod_nbparMo<-aov(Total/Laying_females~(Active_substance+Dose+Clone)^2
-                 +Error(Repetition/Dose/Clone),data=temp)
+                 +Error(Repetition/Dose/Clone),data=MyzHerbiS)
 summary(mod_nbparMo)
 
 #figures for the effect of SA on the mean number of larvae per mother
-interaction.plot(temp$Dose,temp$Active_substance,
-                 temp$Total/temp$Laying_females,las=1)
+interaction.plot(MyzHerbiS$Dose,MyzHerbiS$Active_substance,
+                 MyzHerbiS$Total/MyzHerbiS$Laying_females,las=1)
 boxplot(Total/Laying_females~Clone+Dose+Active_substance,
-        data=temp,las=1,
+        data=MyzHerbiS,las=1,
         at=c(((1+6*0):(6*1))+2*0,
              ((1+6*1):(6*2))+2*1,
              ((1+6*2):(6*3))+2*2,
@@ -237,11 +236,11 @@ boxplot(Total/Laying_females~Clone+Dose+Active_substance,
         border=rep(c(3,4),each=18),
         names=rep(c("NT","NT","N","N","2N","2N"),times=6))
 text(c((0:5)*6+(1:6)*2+1.5),y=5.8,
-     labels=levels(temp$Active_substance),cex=1.5,
+     labels=levels(MyzHerbiS$Active_substance),cex=1.5,
      col=c(3,3,3,4,4,4))
 #since there is no significant effect of the clone, they can be pooled 
 #together
-boxplot(Total/Laying_females~Dose+Active_substance,data=temp,las=1,
+boxplot(Total/Laying_females~Dose+Active_substance,data=MyzHerbiS,las=1,
         at=c(((1+3*0):(3*1))+2*0,
              ((1+3*1):(3*2))+2*1,
              ((1+3*2):(3*3))+2*2,
@@ -251,7 +250,7 @@ boxplot(Total/Laying_females~Dose+Active_substance,data=temp,las=1,
         border=rep(c(3,4),each=9),
         names=rep(c("NT","N","2N"),times=6))
 text(c((0:5)*3+(1:6)*2),y=5.8,
-     labels=levels(temp$Active_substance),cex=1.5,
+     labels=levels(MyzHerbiS$Active_substance),cex=1.5,
      col=c(3,3,3,4,4,4))
 
 
@@ -265,23 +264,23 @@ aggregate(cbind(Live,Total_death,Total)~Clone+Active_substance+
             Dose,data=MyzHerbi,"sum")
 
 MortModl<-glm(cbind(Live,Total_death)~Active_substance*Dose*Clone,
-              binomial,data=temp)
+              binomial,data=MyzHerbiS)
 summary(MortModl)
 anova(MortModl)
 
-r<-temp$Repetition
-ra<-temp$Repetition:temp$Active_substance
-rad<-temp$Repetition:temp$Active_substance:temp$Dose
+r<-MyzHerbiS$Repetition
+ra<-MyzHerbiS$Repetition:MyzHerbiS$Active_substance
+rad<-MyzHerbiS$Repetition:MyzHerbiS$Active_substance:MyzHerbiS$Dose
 
 mmod_Death.2bis<-glmmTMB(cbind(Live,Total_death)~(Active_substance+Dose+Clone)^2+
                         (1|r)+(1|ra)+(1|rad),
-                      data=temp,REML=FALSE,family=binomial)
+                      data=MyzHerbiS,REML=FALSE,family=binomial)
 
 #figures for the effect of SA on the death rate of clones
-interaction.plot(temp$Dose,temp$Active_substance,
-                 temp$Total/temp$Laying_females,las=1)
+interaction.plot(MyzHerbiS$Dose,MyzHerbiS$Active_substance,
+                 MyzHerbiS$Total/MyzHerbiS$Laying_females,las=1)
 boxplot(Total_death/Total~Clone+Dose+Active_substance,
-        data=temp,las=1,
+        data=MyzHerbiS,las=1,
         at=c(((1+6*0):(6*1))+2*0,
              ((1+6*1):(6*2))+2*1,
              ((1+6*2):(6*3))+2*2,
@@ -292,13 +291,13 @@ boxplot(Total_death/Total~Clone+Dose+Active_substance,
         border=rep(c(3,4),each=18),
         names=rep(c("NT","NT","N","N","2N","2N"),times=6))
 text(c((0:5)*6+(1:6)*2+1.5),y=1.02,
-     labels=levels(temp$Active_substance),cex=1.5,
+     labels=levels(MyzHerbiS$Active_substance),cex=1.5,
      col=c(3,3,3,4,4,4))
 #figures for the effect of SA on the number of death for clone (in order
 #to remove the illusion of high death rate computed on a very low number 
 #of individuals)
 boxplot(Total_death~Clone+Dose+Active_substance,
-        data=temp,las=1,
+        data=MyzHerbiS,las=1,
         at=c(((1+6*0):(6*1))+2*0,
              ((1+6*1):(6*2))+2*1,
              ((1+6*2):(6*3))+2*2,
@@ -309,7 +308,7 @@ boxplot(Total_death~Clone+Dose+Active_substance,
         border=rep(c(3,4),each=18),
         names=rep(c("NT","NT","N","N","2N","2N"),times=6))
 text(c((0:5)*6+(1:6)*2+1.5),y=65,
-     labels=levels(temp$Active_substance),cex=1.5,
+     labels=levels(MyzHerbiS$Active_substance),cex=1.5,
      col=c(3,3,3,4,4,4))
 
 
